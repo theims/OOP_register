db9e20b751ed5f520d7a94acc62b0b835ac0d3fb
theims <tuo_pi@hotmail.com>
2014-01-14 15:45:54 +0200

    modified

---
+++ register.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'core/init.php';
+
+if(Input::exists())
+{
+    $validate = new Validate();
+    $validation = $validate->check($_POST, array(
+        'username'          => array(
+            'required'  => true,
+            'min'       => 2,
+            'max'       => 20,
+            'unique'    => 'users'
+        ),
+        'password'          => array(
+            'required'  => true,
+            'min'       => 6
+        ),
+        'password_again'    => array(
+            'required'  => true,
+            'matches'   => 'password'
+        ),
+        'name'              => array(
+            'required'  => true,
+            'min'       => 2,
+            'max'       => 50
+        )
+    ));
+    
+    if($validation->passed())
+    {
+        //rekisteröi käyttäjä
+    }
+    else
+    {
+        //tulosta virheet
+    }
+}
+?>
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Tunnus</label>
+        <input type="text" name="username" id="username" value="" autocomplete="off">
+    </div>
+    <div class="field">
+        <label for="password">Salasana</label>
+        <input type="password" name="password" id="password">
+    </div>
+    <div class="field">
+        <label for="password_again">Salasana uudestaan</label>
+        <input type="password" name="password_again" id="password_again">
+    </div>
+    <div class="field">
+        <label for="name">Nimi</label>
+        <input type="text" name="name" id="name">
+    </div>
+    <input type="submit" value="Rekisteröidy">
+</form>
